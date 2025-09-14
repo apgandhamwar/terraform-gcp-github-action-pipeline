31d1bdee6b3326eabd18d89ce024b9eaaa2a5d47
apgandhamwar <apgandhamwar@gmail.com>
2025-09-14 18:27:33 +0530

    Update gcp-composer.tf

--- terraform/gcp-composer.tf
+++ terraform/gcp-composer.tf
@@ -10,7 +10,7 @@
           disk_size_gb = 10 # Example: Disk size for GKE nodes
         }
         software_config {
-          image_version = "composer-2.b.c-airflow-x.y.z" # Example: Specify Composer image version
+          image_version = composer-2.b.c-airflow-x.y.z
           # Add other software configurations like PyPI packages, environment variables, etc.
         }
         # Add other configuration options as needed, e.g., networking, private IP settings
