3bf20d72071c6deed231bf55f73bac4d398b519a
apgandhamwar <apgandhamwar@gmail.com>
2025-09-14 18:34:23 +0530

    Update gcp-composer.tf

--- terraform/gcp-composer.tf
+++ terraform/gcp-composer.tf
@@ -4,10 +4,10 @@
       project = var.project
 
       config {
-        node_count = 2 
+        node_count = 3
         node_config {
           machine_type = "n1-standard-1"
-          disk_size_gb = 10 
+          disk_size_gb = 10
         }
         software_config {
           image_version = "composer-2-airflow-2"
