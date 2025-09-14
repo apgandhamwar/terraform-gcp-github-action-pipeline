558f9354c6300198390f4b8de6d5e5e5b2de5b86
apgandhamwar <apgandhamwar@gmail.com>
2025-09-14 15:40:00 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
+locals {
+    labels = {
+        "data-project" = var.data-project
+    }
+}
 
 variable "project" {
     type= string
@@ -9,4 +14,9 @@ variable "region" {
     type= string
     description = "Region Google project"
     default = "us1"
+}
+
+variable  "data-project" {
+    type = string
+    description = "Name of data pipeline project to use as resource prefix"
 }
