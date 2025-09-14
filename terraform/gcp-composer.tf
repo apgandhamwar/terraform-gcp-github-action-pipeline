2b68473d4a8cb396a5d68051d501194e81caea76
apgandhamwar <apgandhamwar@gmail.com>
2025-09-14 18:26:05 +0530

    Update gcp-composer.tf

--- terraform/gcp-composer.tf
+++ terraform/gcp-composer.tf
@@ -7,10 +7,10 @@
         node_count = 2 # Example: Number of GKE nodes for the environment
         node_config {
           machine_type = "n1-standard-1" # Example: Machine type for GKE nodes
-          disk_size_gb = 20 # Example: Disk size for GKE nodes
+          disk_size_gb = 10 # Example: Disk size for GKE nodes
         }
         software_config {
-          image_version = "composer-2.x.x-airflow-2.x.x" # Example: Specify Composer image version
+          image_version = "composer-2.b.c-airflow-x.y.z" # Example: Specify Composer image version
           # Add other software configurations like PyPI packages, environment variables, etc.
         }
         # Add other configuration options as needed, e.g., networking, private IP settings
