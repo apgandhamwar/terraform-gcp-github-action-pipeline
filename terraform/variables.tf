1db7399aaede2b81ad9bbe46a70d90ec4921e1b4
apgandhamwar <apgandhamwar@gmail.com>
2025-09-14 15:33:07 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,9 +2,11 @@
 variable "project" {
     type= string
     description = "ID Google project"
+    default = "test"
 }
 
 variable "region" {
     type= string
     description = "Region Google project"
+    default = "us1"
 }
